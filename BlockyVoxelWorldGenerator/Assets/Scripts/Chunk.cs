6f05ce39579af3f85ba8fd62173c99ae7ac5b1b1
Mathieu VIALES <mathieu@viales.fr>
2020-03-28 21:20:00 +0100

    Generate voxel faces one by one

--- BlockyVoxelWorldGenerator/Assets/Scripts/Chunk.cs
+++ BlockyVoxelWorldGenerator/Assets/Scripts/Chunk.cs
@@ -9,14 +9,20 @@ public class Chunk
 
     public Chunk(Vector3 identifier, GameObject parent, WorldGeneratorSettings settings)
     {
+        Identifier = identifier;
+        _settings = settings;
         gameObject = new GameObject
         {
             name = $"Chunk - {identifier.ToString()}"
         };
         gameObject.transform.parent = parent.transform;
+        CreateVoxels(settings);
+        MergeVoxelMeshes();
         gameObject.transform.position = identifier * settings.voxelsPerChunkSide / settings.blocksPerMeter;
-        Identifier = identifier;
-        _settings = settings;
+    }
+
+    private void CreateVoxels(WorldGeneratorSettings settings)
+    {
         Voxels = new Voxel[settings.voxelsPerChunkSide, settings.voxelsPerChunkSide, settings.voxelsPerChunkSide];
         for (var x = 0; x < settings.voxelsPerChunkSide; x++)
         for (var y = 0; y < settings.voxelsPerChunkSide; y++)
@@ -25,13 +31,41 @@ public class Chunk
             var localIdentifier = new Vector3(x, y, z);
             Voxels[x, y, z] = new Voxel(localIdentifier, gameObject.transform.position, gameObject, settings);
         }
-
-        MergeVoxelMeshes();
     }
 
     private void MergeVoxelMeshes()
     {
-        //1. Combine all children meshes
+        var meshCombiner = GetMeshCombinerFromChildren();
+        AddCombinedMeshesToChunk(meshCombiner);
+        AddMeshRenderer();
+        RemoveChildren();
+    }
+
+    private void RemoveChildren()
+    {
+        foreach (Transform quad in gameObject.transform)
+        {
+            GameObject.Destroy(quad.gameObject);
+        }
+    }
+
+    private void AddMeshRenderer()
+    {
+        var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.material = new Material(Shader.Find("Diffuse"));
+    }
+
+    private void AddCombinedMeshesToChunk(CombineInstance[] meshCombiner)
+    {
+        var meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshFilter.mesh = new Mesh();
+        meshFilter.transform.position = Vector3.zero;
+
+        meshFilter.mesh.CombineMeshes(meshCombiner);
+    }
+
+    private CombineInstance[] GetMeshCombinerFromChildren()
+    {
         var voxelMeshFilters = gameObject.GetComponentsInChildren<MeshFilter>();
         var meshCombiner = new CombineInstance[voxelMeshFilters.Length];
         var i = 0;
@@ -42,22 +76,6 @@ public class Chunk
             i++;
         }
 
-        //2. Create a new mesh on the parent object
-        var meshFilter = gameObject.AddComponent<MeshFilter>();
-        meshFilter.mesh = new Mesh();
-        meshFilter.transform.position = Vector3.zero;
-
-        //3. Add combined meshes on children as the parent's mesh
-        meshFilter.mesh.CombineMeshes(meshCombiner);
-
-        //4. Create a renderer for the parent
-        var meshRenderer = gameObject.AddComponent<MeshRenderer>();
-        meshRenderer.material = new Material(Shader.Find("Diffuse"));
-
-        //5. Delete all uncombined children
-        foreach (Transform quad in gameObject.transform)
-        {
-            GameObject.Destroy(quad.gameObject);
-        }
+        return meshCombiner;
     }
 }
--- BlockyVoxelWorldGenerator/Assets/Scripts/Voxel.cs
+++ BlockyVoxelWorldGenerator/Assets/Scripts/Voxel.cs
@@ -1,21 +1,110 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Voxel
 {
     public readonly Vector3 LocalIdentifier;
+    private readonly WorldGeneratorSettings _settings;
     public GameObject gameObject;
+    private readonly Dictionary<Cubeside, Vector3> _sides = new Dictionary<Cubeside, Vector3>
+    {
+        {Cubeside.Back, Vector3.back},
+        {Cubeside.Front, Vector3.forward},
+        {Cubeside.Right, Vector3.right},
+        {Cubeside.Left, Vector3.left},
+        {Cubeside.Up, Vector3.up},
+        {Cubeside.Down, Vector3.down}
+    };
+    
+    enum Cubeside {Down, Up, Left, Right, Front, Back}
 
     public Voxel(Vector3 localIdentifier, Vector3 chunkPosition, GameObject parent, WorldGeneratorSettings settings)
     {
-        gameObject = new GameObject();
-        gameObject.name = $"Voxel - {localIdentifier.ToString()}";
-        gameObject.transform.parent = parent.transform;
-        gameObject.transform.position =  (localIdentifier / settings.blocksPerMeter) + chunkPosition ;
-        gameObject.transform.localScale = Vector3.one / settings.blocksPerMeter;
         LocalIdentifier = localIdentifier;
-        var primitive = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        gameObject.AddComponent<MeshFilter>().mesh = primitive.GetComponent<MeshFilter>().mesh;
-        gameObject.AddComponent<MeshRenderer>().material = primitive.GetComponent<MeshRenderer>().material;
-        GameObject.Destroy(primitive);
+        _settings = settings;
+        CreateGameObject(localIdentifier, chunkPosition, parent);
+        CreateMesh();
+    }
+
+    private void CreateGameObject(Vector3 localIdentifier, Vector3 chunkPosition, GameObject parent)
+    {
+        gameObject = new GameObject
+        {
+            name = $"Voxel - {localIdentifier.ToString()}"
+        };
+        gameObject.transform.parent = parent.transform;
+        gameObject.transform.position = (localIdentifier / _settings.blocksPerMeter) + chunkPosition;
+        gameObject.transform.localScale = Vector3.one / _settings.blocksPerMeter;
+    }
+
+    private void CreateMesh()
+    {
+        foreach (var side in _sides)
+        {
+            CreateFace(side.Key, side.Value);
+        }
+    }
+
+    private void CreateFace(Cubeside sideName, Vector3 sideDirection)
+    {
+        var mesh = new Mesh
+        {
+            name = "ScriptedMesh" + sideName
+        };
+
+        Vector3[] vertices;
+        var normals = new[] {sideDirection, sideDirection, sideDirection, sideDirection};
+        var triangles = new[] { 3, 1, 0, 3, 2, 1};
+        Vector2[] uvs = {Vector2.down, Vector2.left, Vector2.up, Vector2.right};
+        // Bottom
+        var p0 = new Vector3( -0.5f,  -0.5f,  0.5f );
+        var p1 = new Vector3(  0.5f,  -0.5f,  0.5f );
+        var p2 = new Vector3(  0.5f,  -0.5f, -0.5f );
+        var p3 = new Vector3( -0.5f,  -0.5f, -0.5f );	
+        // Top
+        var p4 = new Vector3( -0.5f,   0.5f,  0.5f );
+        var p5 = new Vector3(  0.5f,   0.5f,  0.5f );
+        var p6 = new Vector3(  0.5f,   0.5f, -0.5f );
+        var p7 = new Vector3( -0.5f,   0.5f, -0.5f );
+
+        switch (sideName)
+        {
+            case Cubeside.Down:
+                vertices = new[] {p0, p1, p2, p3};
+                break;
+            case Cubeside.Up:
+                vertices = new[] {p7, p6, p5, p4};
+                break;
+            case Cubeside.Left:
+                vertices = new[] {p7, p4, p0, p3};
+                break;
+            case Cubeside.Right:
+                vertices = new[] {p5, p6, p2, p1};
+                break;
+            case Cubeside.Front:
+                vertices = new[] {p4, p5, p1, p0};
+                break;
+            case Cubeside.Back:
+                vertices = new[] {p6, p7, p3, p2};
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(sideName), sideName, null);
+        }
+        
+        
+        mesh.vertices = vertices;
+        mesh.normals = normals;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+		 
+        mesh.RecalculateBounds();
+		
+        var quad = new GameObject("Quad");
+        quad.transform.parent = gameObject.transform;
+        quad.transform.position = LocalIdentifier;
+
+        var meshFilter = quad.AddComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
     }
 }
